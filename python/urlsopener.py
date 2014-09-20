d9f2bc3a4f3664680810590f08820a5192681d5c
homker <liu15jia@163.com>
2014-09-20 22:05:32 +0800

    fix

--- python/urlsopener.py
+++ python/urlsopener.py
@@ -5,11 +5,15 @@ import urllib
 import re
 import thread
 import time
+import HTMLParser
+from htmlentitydefs import entitydefs
 
 class spider_model:
+
 	def __init__(self):
 		self.enable = False
 		self.url = "http://www.ecjtu.net/html/ecjtunews1/"
+		
 	def GetPage(self,url):
 		myUrl = url 
 		user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
@@ -19,24 +23,76 @@ class spider_model:
 		myPage = myResponse.read()
 		unicodePage = myPage.decode("gbk")
 		#print type(myPage).
-		parttern = '<div.*?id="bigtitle".*?title="(.*?)">(.*?)</div>'
-		myItem = re.findall(parttern, unicodePage,re.S)
+		'''
+		partternB = '<div.*?id="bigtitle".*?href="(.*?)".*?>(.*?)</a>'
+		partternG = '<a *?href="(.*?)".*?>(.*?)</a>'
+		myItem = re.findall(partternG, unicodePage,re.S)
 		items = []
 		for item in myItem:
-			items.append([item[0].replace("\n",""),item[1].replace("\n","")])
+			items.append(['http://www.ecjtu.net'+item[0],item[1].replace("\n","")])
 		return items
+		'''
+		return unicodePage
 		
 	def Start(self):
 		self.enable = True
 		print u'loding...'
 		result = self.GetPage(self.url)
-		result = result
-		for item in result:
-			for itm in item:
-				print itm
+		return result
+
+class parser_model(HTMLParser.HTMLParser):
+
+	def __init__(self):
+		self.taglevels = []
+		self.handledtags = ['a']
+		self.precessing = None
+		self.linkstring = ''
+		self.linkaddr = ''
+		HTMLParser.HTMLParser.__init__(self)
+		
+	def handle_starttag(self, tag, attrs):
+		if tag in self.handledtags:
+			for name,value in attrs:
+				if name == 'href':
+					if value.find('http://') = -1
+					print u'http://www.ecjtu.net'+value
+			self.processing = tag
+
+	def handle_data(self,data):
+		if self.precessing:
+			self.linkstring += data
+			
+	def handle_endtag(self,tag):
+		if tag == self.precessing:
+			print self.linkstring.decode('utf-8')+':'+self.linkaddr
+			self.precessing = None
+			self.linkstring = ''
+			
+	def handle_entityref(self, name):
+		if entitydefs.has_key(name):
+			self.handle_data(entitydefs[name])
+		else:
+			self.handle_data('&'+name+';')
+			
+	def handle_charref(self,name):
+		try:
+			charnum = int(name)
+		except ValueError:
+			return
+		if charnum<1 or charnum>255:
+			return
+		self.handle_data(chr(charnum))
+	
+	def gettitle(self):
+		return self.linkaddr
+		
+		
+
 print u""" 开始嘛？quit退出"""
 print u'enter start'
 raw_input(' ')
 myModel = spider_model()
-myModel.Start()
+page = myModel.Start()
+content = parser_model()
+content.feed(page)
 
