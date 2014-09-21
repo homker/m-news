175c772863ac2b386d0f37dc19efdac9457771bb
homker <liu15jia@163.com>
2014-09-21 22:53:55 +0800

    fix server

--- python/server.py
+++ python/server.py
@@ -1,3 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from BaseHTTPServer import HTTPServer
+import sys
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+
+class TestHTTPHandle(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        buf = 'It works'
+        print buf
+        self.protocal_version = "HTTP/1.1" 
+        self.send_response(200)
+
+        self.send_header("Welcome", "Contect")       
+
+        self.end_headers()
+
+        self.wfile.write(buf)
+
+def start_server(port):
+    http_server = HTTPServer(('', int(port)), TestHTTPHandle)
+    print 'started httpserver'
+    http_server.serve_forever() 
+start_server(8000)
+
+
